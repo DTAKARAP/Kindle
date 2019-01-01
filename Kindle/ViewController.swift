920652ed2db5732ea5dcfeca21e1b27716c310a9
Divya Tejasvi <divya@divyas-macbook-pro.local>
2019-01-01 16:33:05 -0500

    Initial Commit

---
+++ Kindle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kindle
+//
+//  Created by Divya Tejasvi on 1/1/19.
+//  Copyright © 2019 Divya Tejasvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ KindleTests/KindleTests.swift
@@ -0,0 +1,34 @@
+//
+//  KindleTests.swift
+//  KindleTests
+//
+//  Created by Divya Tejasvi on 1/1/19.
+//  Copyright © 2019 Divya Tejasvi. All rights reserved.
+//
+
+import XCTest
+@testable import Kindle
+
+class KindleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
